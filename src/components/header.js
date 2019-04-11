3370dc0bb448186b39c3e71f01100e2966928203
rezart mazellari <rezartmazellari@Tony.local>
2019-04-10 17:20:32 -0700

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,22 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from '../images/gatsby-icon.png'
+
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+  <header className="header">
+    <div>
       <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+        <Link to="/">
+          <img className="logo" src={logo} alt="logo" />
+          <div className="title">West Coast </div>
+          <div className="title-2">Plaster</div>
         </Link>
       </h1>
+      <div className="contact-btn">
+        <a href="#">Say Hello!</a>
+      </div>
     </div>
   </header>
 )
